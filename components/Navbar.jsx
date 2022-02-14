562ad6cb17b9b81de1871bcb9ce912d2745fbb63
Reedemer <helloreedemer@gmail.com>
2022-02-14 14:16:47 +0200

    created template

---
+++ components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav style={{color:"white", padding:"20px 30px"}}>
+         My navbar
+    </nav>
+  )
+}
+
+export default Navbar
