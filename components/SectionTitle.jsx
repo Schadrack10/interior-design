07ef768738a4ab0476b2176799235ee6b5d6a0e8
Reedemer <helloreedemer@gmail.com>
2022-02-14 15:48:27 +0200

    finished merged service section

--- components/SectionTitle.jsx
+++ components/SectionTitle.jsx
@@ -3,11 +3,11 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 
 
-const SectionTitle = (props) => {
+const SectionTitle = ({title}) => {
   return (
       <Box sx={{display:"flex",flexDirection:"column",justifyContent:"flex-start"}}>
     <h1 style={{color:"#f44336"}}>
-        {props.title}
+        {title}
     </ h1>
     <Box  style={{background:"#f44336",height:"5px",border:"none",borderRadius:"20px" , width:"100px"}}></Box>
     </Box>
